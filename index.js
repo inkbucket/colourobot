3990051945a902f5a9fccf46205af89f2a2235c7
lokesh_coder <mexican.dirtyfellow@gmail.com>
2019-01-09 23:43:27 +0530

    create screenshor once google font is loaded

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const tpl = require("./template");
 
 var T = new Twit(config);
 
-function uploadImage(imagePath, hashTag) {
+function uploadImage(imagePath, hashTag, res) {
   var b64content = fs.readFileSync(imagePath, { encoding: "base64" });
+  console.log("uploadImage: start");
 
   T.post("media/upload", { media_data: b64content }, function(err, data, response) {
     var mediaIdStr = data.media_id_string;
     var altText = new Date().toISOString();
     var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+    console.log("uploadImage: create");
 
     T.post("media/metadata/create", meta_params, function(err, data, response) {
       if (!err) {
@@ -22,9 +24,15 @@ function uploadImage(imagePath, hashTag) {
           status: hashTag,
           media_ids: [mediaIdStr]
         };
+        console.log("uploadImage: upload");
 
         T.post("statuses/update", params, function(err, data, response) {
-          // console.log(data);
+          if (err) {
+            console.log("Error", err);
+            return;
+          }
+          console.log("uploadImage: done");
+          res.end("screenshot done");
         });
       }
     });
@@ -32,23 +40,27 @@ function uploadImage(imagePath, hashTag) {
 }
 
 module.exports = async (req, res) => {
-  const browser = await puppeteer.launch({
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 250, height: 264, deviceScaleFactor: 1.3 });
+  try {
+    const browser = await puppeteer.launch({
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 250, height: 264, deviceScaleFactor: 1.3 });
 
-  fs.writeFileSync(__dirname + "./tpl.html", await tpl());
-  fs.writeFileSync(__dirname + "./tpl.html", await tpl());
-  await page.setContent(await tpl());
-  let hexEl = await page.$(".hex");
-  let hexValue = await page.evaluate(element => element.textContent, hexEl);
-  const screenshot = await page.screenshot({ path: "./example.png" });
-  uploadImage("./example.png", hexValue);
+    await page.setContent(await tpl(), {
+      timeout: 0,
+      waitUntil: "networkidle0"
+    });
+    let hexEl = await page.$(".hex");
+    let hexValue = await page.evaluate(element => element.textContent, hexEl);
+    await page.screenshot({ path: "/tmp/example.png" });
+    uploadImage("/tmp/example.png", hexValue, res);
 
-  await browser.close();
-  res.end("screenshot done");
-  res.end(screenshot);
+    await browser.close();
+  } catch (e) {
+    console.log(e);
+    res.end("Got Error!");
+  }
 };

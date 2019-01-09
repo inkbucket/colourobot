6146a0be506284b60174ab0ec892dced3c0938e5
lokesh_coder <mexican.dirtyfellow@gmail.com>
2019-01-09 23:42:47 +0530

    remove unused data

--- chroma.js
+++ chroma.js
@@ -10,64 +10,8 @@ var color = chroma
 
 color = chroma(color);
 
-const trimDecimals = function(num) {
-  return num.toFixed(1);
-};
-
 var data = {
-  hex: color.hex(),
-  rgb: color.rgb().join(" , "),
-  hsl: color
-    .hsl()
-    .map(a => trimDecimals(a))
-    .join(" , "),
-  classes: chroma
-    .scale([color, chroma(color).set("hsl.l", "+20")])
-    .mode("lch")
-    .colors(6)
-    .slice(0, -1),
-  tones: [
-    {
-      name: "saturate",
-      color: chroma(color)
-        .saturate()
-        .hex()
-    },
-    {
-      name: "saturate2",
-      color: chroma.blend(color, chroma(color).set("hsl.h", chroma(color).get("hsl.h") + 10), "overlay").hex()
-    },
-    {
-      name: "brighten",
-      color: chroma(color)
-        .brighten()
-        .hex()
-    },
-    {
-      name: "lightness",
-      color: chroma(color)
-        .luminance(0.5)
-        .hex()
-    },
-    {
-      name: "darken",
-      color: chroma(color)
-        .darken()
-        .hex()
-    }
-  ],
-  name: chroma(color).name(),
-  range: chroma
-    .cubehelix()
-    .start(chroma(color).get("hsl.h"))
-    .rotations(-0.35)
-    .gamma(0.5)
-    .lightness([0.4, 0.8])
-    .scale()
-    .correctLightness()
-    .colors(5),
-  temperature: chroma(color).temperature(),
-  contrast: chroma.contrast(color, "white")
+  hex: color.hex()
 };
 
 exports.module = data;

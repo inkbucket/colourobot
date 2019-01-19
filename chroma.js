2f1b66f678a0aed9b61b6faea4f9c4de33fe4192
lokesh_coder <mexican.dirtyfellow@gmail.com>
2019-01-19 23:25:05 +0530

    update color gen logic

--- chroma.js
+++ chroma.js
@@ -1,12 +1,20 @@
 var chroma = require("chroma-js");
 
-var color = chroma
+var randomVal = parseFloat(Math.random().toFixed(2));
+
+if (randomVal == 0) {
+  randomVal = 0.45;
+}
+
+var colors = chroma
   .cubehelix()
   .start(chroma(chroma.random()).get("hsl.h"))
-  .gamma(0.9)
-  .lightness([0.1, 0.9])
+  .gamma(randomVal)
+  .lightness([randomVal, randomVal])
   .scale()
-  .colors(1)[0];
+  .colors(10);
+var randomIndex = Math.round(Math.random() * 9);
+var color = colors[randomIndex];
 
 color = chroma(color);
 
